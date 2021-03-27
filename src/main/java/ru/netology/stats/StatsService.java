46a47eb02f3046f7a5b6c32d25c7f6d17e06d688
Bair B <bairu@yandex.ru>
2021-03-27 19:37:41 +0300

    files mod

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,43 +1,76 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long calculateSum(int[] months) {
-        long sum = 0;
+    public int calculateSum(int[] months) {
+        int sum = 0;
         for (int month : months) {
             sum += month;
         }
         return sum;
     }
 
-    public long calculateAvg(int[] months) {
-        long sum = 0;
-        for (int month : months) {
-            sum += month;
-        }
-        int avg = (int) sum / 12;
+    public int calculateAvg(int[] months) {
+        int sum = calculateSum(months);
+        int avg = sum / months.length;
         return avg;
     }
 
-    public int findMax(int[] months) {
-        int month = 0;
-        for (int i = 0; i < months.length; i++) {
-            if (months[i] >= months[month]) {
-                month = i;
+    public int findMaxInMonth(int[] months) {
+        int max = months[0];
+        for (int month : months) {
+            if (month > max) {
+                max = month;
             }
         }
-        return month + 1;
-    }
-
-    public int findMin(int[] months) {
-        int month = 0;
-        for (int i = 0; i < months.length; i++) {
-            if (months[i] < months[month]) {
-                month = i;
+        int number = 0;
+        int monthNumber = 0;
+        for (int month : months) {
+            number++;
+            if (month == max) {
+                monthNumber = number;
             }
         }
-        return month + 1;
+        return monthNumber;
     }
 
+    public int findMinInMonth(int[] months) {
+        int min = months[0];
+        for (int month : months) {
+            if (month < min) {
+                min = month;
+            }
+        }
+        int number = 0;
+        int monthNumber = 0;
+        for (int month : months) {
+            number++;
+            if (month == min) {
+                monthNumber = number;
+            }
+        }
+        return monthNumber;
+    }
+
+    public int amountMonthsLessThanAvg(int months[]) {
+        int amount = 0;
+        int avg = calculateAvg(months);
+        for (int month : months) {
+            if (month < avg) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+
+    public int amountMonthsMoreThanAvg(int months[]) {
+        int amount = 0;
+        int avg = calculateAvg(months);
+        for (int month : months) {
+            if (month > avg) {
+                amount++;
+            }
+        }
+        return amount;
+    }
 }
-
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -9,35 +9,48 @@ class StatsServiceTest {
     void calculateSum() {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        //int expected = 180;
-        //int actual = (int) service.calculateSum(months);
-        //assertEquals(expected, actual);
-        int monthsSum = (int) service.calculateSum(months);
-        System.out.println("Сумма всех продаж равна: " + monthsSum);
+        int sum = service.calculateSum(months);
+        System.out.println("Сумма всех продаж равна: " + sum);
     }
 
     @Test
     void calculateAvg() {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int monthsAvg = (int) service.calculateAvg(months);
-        System.out.println("Средняя сумма продаж в месяц равна: " + monthsAvg);
-    }
-
-
-    @Test
-    void findMax() {
-        StatsService service = new StatsService();
-        int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int monthMax = (int) service.findMax(months);
-        System.out.println(monthMax);
+        int avg = service.calculateAvg(months);
+        System.out.println("Средняя сумма продаж в месяц равна: " + avg);
     }
 
     @Test
-    void findMin() {
+    void findMaxInMonth() {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int monthMin = (int) service.findMin(months);
-        System.out.println(monthMin);
+        int maxInMonth = service.findMaxInMonth(months);
+        System.out.println("Номер месяца с максимальной продажей: " + maxInMonth);
+    }
+
+    @Test
+    void findMinInMonth() {
+        StatsService service = new StatsService();
+        int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int minInMonth = service.findMinInMonth(months);
+        System.out.println("Номер месяца с минимальной продажей: " + minInMonth);
+    }
+
+    @Test
+    void amountMonthsLessThanAvg() {
+        StatsService service = new StatsService();
+        int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int amounts = service.amountMonthsLessThanAvg(months);
+        System.out.println("Кол-во месяцев, в которых продажи были ниже среднего: " + amounts);
+    }
+
+    @Test
+    void amountMonthsMoreThanAvg() {
+        StatsService service = new StatsService();
+        int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int amounts = service.amountMonthsMoreThanAvg(months);
+        System.out.println("Кол-во месяцев, в которых продажи были выше среднего: " + amounts);
     }
 }
+
