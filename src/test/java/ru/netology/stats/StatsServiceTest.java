2cf0316bf06911b3ac269dc7f4fee053293b0293
Bair B <bairu@yandex.ru>
2021-03-30 12:37:00 +0300

    files mod

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -10,7 +10,13 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int sum = service.calculateSum(months);
-        System.out.println("Сумма всех продаж равна: " + sum);
+        int expected = 180;
+        int actual = service.calculateSum(months);
+        assertEquals(expected, actual);
+//        int expected = 180;
+//        int actual = service.calculateSum(months);
+//        boolean passed = actual == expected;
+//        System.out.println(passed);
     }
 
     @Test
@@ -18,7 +24,9 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int avg = service.calculateAvg(months);
-        System.out.println("Средняя сумма продаж в месяц равна: " + avg);
+        int expected = 15;
+        int actual = service.calculateAvg(months);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,7 +34,9 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int maxInMonth = service.findMaxInMonth(months);
-        System.out.println("Номер месяца с максимальной продажей: " + maxInMonth);
+        int expected = 8;
+        int actual = service.findMaxInMonth(months);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -34,7 +44,9 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int minInMonth = service.findMinInMonth(months);
-        System.out.println("Номер месяца с минимальной продажей: " + minInMonth);
+        int expected = 9;
+        int actual = service.findMinInMonth(months);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -42,7 +54,9 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int amounts = service.amountMonthsLessThanAvg(months);
-        System.out.println("Кол-во месяцев, в которых продажи были ниже среднего: " + amounts);
+        int expected = 5;
+        int actual = service.amountMonthsLessThanAvg(months);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -50,7 +64,9 @@ class StatsServiceTest {
         StatsService service = new StatsService();
         int[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int amounts = service.amountMonthsMoreThanAvg(months);
-        System.out.println("Кол-во месяцев, в которых продажи были выше среднего: " + amounts);
+        int expected = 5;
+        int actual = service.amountMonthsMoreThanAvg(months);
+        assertEquals(expected, actual);
     }
 }
 
